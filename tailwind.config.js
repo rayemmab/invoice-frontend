f8487338539f5c9a969caf38357db48cb0b39fc5
rayemmab <raybouss30@gmail.com>
2025-04-06 18:22:47 +0100

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#4F46E5',
+          light: '#6366F1',
+          dark: '#4338CA',
+        },
+        secondary: {
+          DEFAULT: '#10B981',
+          light: '#34D399',
+          dark: '#059669',
+        },
+        danger: {
+          DEFAULT: '#EF4444',
+          light: '#F87171',
+          dark: '#DC2626',
+        },
+      },
+    },
+  },
+}
